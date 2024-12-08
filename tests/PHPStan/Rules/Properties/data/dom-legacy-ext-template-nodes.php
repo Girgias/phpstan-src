1bdd8760db0b661d86363f1df0b99e8330467c57
Gina Peter Banyard <girgias@php.net>
2024-12-08 17:13:55 +0000

    Revert "Try to parameterrize DOMAttr"
    This reverts commit 3d93d420b54657983f329c5408bab9259daa551b.

--- tests/PHPStan/Rules/Properties/data/dom-legacy-ext-template-nodes.php
+++ tests/PHPStan/Rules/Properties/data/dom-legacy-ext-template-nodes.php
@@ -3,24 +3,16 @@
 use function PHPStan\Testing\assertType;
 
 function basic_node(\DOMNode $node): void {
-	if ($node->hasAttributes()) {
-		assertType(DOMNamedNodeMap::class . '<' . DOMAttr::class . '>', $node->attributes);
-	} else {
-		assertType('null', $node->attributes);
-	}
+	assertType(DOMNamedNodeMap::class . '<' . DOMAttr::class . '>', $node->attributes);
 };
 
-function element_node(\DOMElement $element): void
+function element_node(\DOMElement $node): void
 {
-	if ($element->hasAttribute('class')) {
-		assertType(DOMNamedNodeMap::class . '<' . DOMAttr::class . '>', $element->attributes);
-		$attribute = $element->attributes->getNamedItem('class');
-		if ($attribute === null) {
-			return;
-		}
-		assertType(DOMAttr::class, $attribute);
-		assertType('string', $attribute->value);
-	} else {
-		assertType('null', $element->attributes);
+	assertType(DOMNamedNodeMap::class . '<' . DOMAttr::class . '>', $node->attributes);
+	$attribute = $node->attributes->getNamedItem('class');
+	if ($attribute === null) {
+		return;
 	}
+	assertType(DOMAttr::class, $attribute);
+	assertType('string', $attribute->value);
 }
