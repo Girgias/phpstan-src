a874a1f7f04b6bad40b7740ac4422136c5218f1a
Gina Peter Banyard <girgias@php.net>
2024-12-08 17:03:24 +0000

    Try to parameterrize DOMAttr

--- tests/PHPStan/Rules/Properties/data/dom-legacy-ext-template-nodes.php
+++ tests/PHPStan/Rules/Properties/data/dom-legacy-ext-template-nodes.php
@@ -3,16 +3,24 @@
 use function PHPStan\Testing\assertType;
 
 function basic_node(\DOMNode $node): void {
-	assertType(DOMNamedNodeMap::class . '<' . DOMAttr::class . '>', $node->attributes);
+	if ($node->hasAttributes()) {
+		assertType(DOMNamedNodeMap::class . '<' . DOMAttr::class . '>', $node->attributes);
+	} else {
+		assertType('null', $node->attributes);
+	}
 };
 
-function element_node(\DOMElement $node): void
+function element_node(\DOMElement $element): void
 {
-	assertType(DOMNamedNodeMap::class . '<' . DOMAttr::class . '>', $node->attributes);
-	$attribute = $node->attributes->getNamedItem('class');
-	if ($attribute === null) {
-		return;
+	if ($element->hasAttribute('class')) {
+		assertType(DOMNamedNodeMap::class . '<' . DOMAttr::class . '>', $element->attributes);
+		$attribute = $element->attributes->getNamedItem('class');
+		if ($attribute === null) {
+			return;
+		}
+		assertType(DOMAttr::class, $attribute);
+		assertType('string', $attribute->value);
+	} else {
+		assertType('null', $element->attributes);
 	}
-	assertType(DOMAttr::class, $attribute);
-	assertType('string', $attribute->value);
 }
