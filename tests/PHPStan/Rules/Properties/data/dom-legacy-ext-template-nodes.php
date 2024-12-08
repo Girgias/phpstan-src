156201f927e29f6ad9e403809e960b4b29e82e71
Gina Peter Banyard <girgias@php.net>
2024-12-08 17:21:33 +0000

    DOMNode::hasAttributes() assertions

--- tests/PHPStan/Rules/Properties/data/dom-legacy-ext-template-nodes.php
+++ tests/PHPStan/Rules/Properties/data/dom-legacy-ext-template-nodes.php
@@ -3,16 +3,9 @@
 use function PHPStan\Testing\assertType;
 
 function basic_node(\DOMNode $node): void {
-	assertType(DOMNamedNodeMap::class . '<' . DOMAttr::class . '>', $node->attributes);
-};
-
-function element_node(\DOMElement $node): void
-{
-	assertType(DOMNamedNodeMap::class . '<' . DOMAttr::class . '>', $node->attributes);
-	$attribute = $node->attributes->getNamedItem('class');
-	if ($attribute === null) {
-		return;
+	if ($node->hasAttributes()) {
+		assertType(DOMNamedNodeMap::class . '<' . DOMAttr::class . '>', $node->attributes);
+	} else {
+		assertType('null', $node->attributes);
 	}
-	assertType(DOMAttr::class, $attribute);
-	assertType('string', $attribute->value);
-}
+};
