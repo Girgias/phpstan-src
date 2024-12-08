d7ecb21308ebdeff406a971250632165a44929d3
Gina Peter Banyard <girgias@php.net>
2024-12-08 21:32:49 +0000

    Tests updates prior to ondrej

--- tests/PHPStan/Analyser/nsrt/DOMLegacyNamedNodeNap.php
+++ tests/PHPStan/Analyser/nsrt/DOMLegacyNamedNodeNap.php
@@ -5,18 +5,45 @@ declare(strict_types = 1);
 namespace DOMLegacyNamedNodeMap;
 
 use DOMAttr;
+use DOMElement;
 use DOMNode;
 use DOMNamedNodeMap;
 use function PHPStan\Testing\assertType;
 
 class Foo
 {
-	public function checkAttributes(DOMNode $node): void {
-		assertType('DOMNamedNodeMap<DOMAttr>', $node->attributes);
-		if ($node->hasAttribute('attr')) {
-			assertType('DOMAttr', $node->attributes->getNamedItem('attr'));
+	public function basic_node(DOMNode $node): void {
+		if ($node->hasAttributes()) {
+			assertType('DOMNamedNodeMap<DOMAttr>', $node->attributes);
 		} else {
-			assertType('null', $node->attributes->getNamedItem('attr'));
+			assertType('null', $node->attributes);
+		}
+	}
+
+	public function element_node(DOMElement $element): void
+	{
+		if ($element->hasAttribute('class')) {
+			assertType('DOMNamedNodeMap<DOMAttr>', $element->attributes);
+			$attribute = $element->getAttributeNode('class');
+			assertType(DOMAttr::class, $attribute);
+			assertType('string', $attribute->value);
+		} else {
+			assertType('DOMNamedNodeMap<DOMAttr>|null', $element->attributes);
+		}
+	}
+
+	public function element_node_attribute_fetch_via_attributes_property(DOMElement $element): void
+	{
+		if ($element->hasAttribute('class')) {
+			assertType('DOMNamedNodeMap<DOMAttr>', $element->attributes);
+			$attribute = $element->attributes->getNamedItem('class');
+			if ($attribute === null) {
+				return;
+			}
+			assertType(DOMAttr::class, $attribute);
+			assertType('string', $attribute->value);
+		} else {
+			assertType('DOMNamedNodeMap<DOMAttr>|null', $element->attributes);
 		}
 	}
 }
--- tests/PHPStan/Rules/Properties/data/dom-legacy-ext-template-nodes.php
+++ tests/PHPStan/Rules/Properties/data/dom-legacy-ext-template-nodes.php
@@ -8,4 +8,31 @@ function basic_node(\DOMNode $node): void {
 	} else {
 		assertType('null', $node->attributes);
 	}
-};
+}
+
+function element_node(\DOMElement $element): void
+{
+	if ($element->hasAttribute('class')) {
+		assertType(DOMNamedNodeMap::class . '<' . DOMAttr::class . '>', $element->attributes);
+		$attribute = $element->getAttributeNode('class');
+		assertType(DOMAttr::class, $attribute);
+		assertType('string', $attribute->value);
+	} else {
+		assertType('DOMNamedNodeMap::class . '<' . DOMAttr::class . '>'|null', $element->attributes);
+	}
+}
+
+function element_node_attribute_fetch_via_attributes_property(\DOMElement $element): void
+{
+	if ($element->hasAttribute('class')) {
+		assertType(DOMNamedNodeMap::class . '<' . DOMAttr::class . '>', $element->attributes);
+		$attribute = $element->attributes->getNamedItem('class');
+		if ($attribute === null) {
+			return;
+		}
+		assertType(DOMAttr::class, $attribute);
+		assertType('string', $attribute->value);
+	} else {
+		assertType('DOMNamedNodeMap::class . '<' . DOMAttr::class . '>'|null', $element->attributes);
+	}
+}
